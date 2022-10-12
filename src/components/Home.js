d65cb6ae2c41c0cbd2532f790928ca17dc7b2d7e
Raquel <raquel.panapalen@gmail.com>
2022-10-12 19:40:49 +0200

    home page updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/foto_inicial.svg';
+import '../styles/Home.css';
 
 export default function Home() {
-    return <div>Home</div>
+  return (
+    <div className="home-grid">
+      <div className="home01">
+        <span className="title">
+          Your<span className="text1">Own</span>Voice
+        </span>
+      </div>
+      <div className="home02">
+        <img src={logo} alt="home tts icon" height={400} />
+      </div>
+      <div className="home03">
+        YourOwnVoice uses{' '}
+        <a href="https://github.com/coqui-ai/TTS" style={{ color: 'white' }}>
+          CoquiTTS
+        </a>{' '}
+        technology for Text-to-Speech generation. <br /> This tool allows the
+        user to record their own voice or upload an audio file, <br /> choose
+        the desired speech model and synthesise a personalized input text.
+      </div>
+      <div className="home04">
+        <a href="/tts-generator">
+          <button>
+            Start creating <br /> Your<span className="text1">Own</span>Voice
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 }
-
