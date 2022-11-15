70cd85bed914101753faedd8182f3fa778659e20
sergiespinosa01 <100561275+sergiespinosa01@users.noreply.github.com>
2022-11-15 13:32:51 +0100

    Add files via upload

---
+++ src/components/SocialResponsabilities.js
@@ -0,0 +1,52 @@
+import "../style/SocialResponsabilities.css";
+import React from "react";
+
+export default function SocialResponsabilities() {
+  return (
+    <>
+      <div className="container">
+        <div className="title"> Social Responsabilities</div>
+        <div className="subtitle1"> <u> Legal and Ethical Aspects </u> </div>
+
+        <p className="text1">
+          
+           Your Own Voice is an initiative from the
+          teacher José Adrián Fonollosa of the subject “Projecte Avançat
+          d’Enginyeria” at Universitat Politècnica de Catalunya and carried out
+          by students coursing it. This project works respecting the General
+          Data Protection Regulation (EU 2016/679). The responsibility of the
+          treatment of the data is the Your Own Voice team. The goal of this
+          treatment is to obtain synthetic voices in order to use them, for
+          example, by people with speech issues. The personal data requested is
+          only the email. Cessions: No data will be transferred except as
+          provided by law. The rights over your data are those of access,
+          deletion, rectification, opposition, limitation of processing,
+          portability and oblivion. You can exercise them by sending your
+          request to the team leader, at this email address
+          sara.tarrats@estudiantat.upc.edu . As a company we assure not to make
+          any use of this data without the explicit consent of the user. We do
+          not allow minors to upload personal data, as it is explicitly
+          indicated on the web. Before publishing a voice we will check that it
+          is not a child's voice. An ethical important value we have as a
+          company is that we take care of having both men and women in the
+          models. We commit ourselves to offer a representative service for all
+          the population. 
+          </p>
+
+          <div className="subtitle2"> <u> Sustainability </u> </div>
+          <p className="text2">
+          
+          Even if it is true that the energy
+          consumption caused by our computers and the server is not despicable,
+          we are proud because it is not too high and has acceptable values. We
+          believe that the amount of good we make for society compensates this
+          negative aspect about energy consumption. In fact, we are happy to
+          announce that our estimate is that in 2024 this consumption will be
+          reduced by 50%. 
+         </p>
+
+        
+      </div>
+    </>
+  );
+}
